978ecb779cbdec14d9c145e80c36ea34a67de6a7
Supi <supimon@gmail.com>
2020-01-03 08:59:26 +0530

    first commit

---
+++ src/main/java/com/supimon/cheflistservice/ChefListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supimon.cheflistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefListServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supimon/cheflistservice/ChefListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supimon.cheflistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
