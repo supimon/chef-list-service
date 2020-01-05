eb0997714d52b1159eee4df95d8328e1464af68b
Supi <supimon@gmail.com>
2020-01-05 11:39:08 +0530

    added firebase support

--- src/main/java/com/supimon/cheflistservice/resource/ChefListService.java
+++ src/main/java/com/supimon/cheflistservice/resource/ChefListService.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,7 +16,10 @@ import java.util.List;
 public class ChefListService {
 
     @RequestMapping("/{filter}")
-    public ChefListWrapper getChefs(@PathVariable("filter") String filter){
+    public ChefListWrapper getChefs(@PathVariable("filter") String filter) throws FileNotFoundException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/chefapp-eeae0-firebase-adminsdk-tujtr-198a71e00a.json");
 
         List<ChefItem> chefs = Arrays.asList(
 
