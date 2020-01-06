27e7fdbaed30b413b5e048bfde6fcf510470c453
Supi <supimon@gmail.com>
2020-01-06 21:14:04 +0530

    edited the models to correct the types and moved the FirebaseApp initialisation to main method so its only initialised once. POC done - yay!

--- src/main/java/com/supimon/cheflistservice/resource/ChefListService.java
+++ src/main/java/com/supimon/cheflistservice/resource/ChefListService.java
@@ -1,12 +1,9 @@
 package com.supimon.cheflistservice.resource;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.supimon.cheflistservice.models.ChefItem;
 import com.supimon.cheflistservice.models.ChefListWrapper;
@@ -14,8 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -25,17 +20,7 @@ import java.util.concurrent.ExecutionException;
 public class ChefListService {
 
     @RequestMapping("/{filter}")
-    public ChefListWrapper getChefs(@PathVariable("filter") String filter) throws IOException, InterruptedException, ExecutionException {
-
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/chefapp-eeae0-firebase-adminsdk-tujtr-198a71e00a.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://chefapp-eeae0.firebaseio.com")
-                .build();
-
-        FirebaseApp.initializeApp(options);
+    public ChefListWrapper getChefs(@PathVariable("filter") String filter) throws InterruptedException, ExecutionException {
 
         Firestore db = FirestoreClient.getFirestore();
 
